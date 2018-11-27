5060036de633707eb127ae0835c28fb8c57a5966
Emmanuell de Araujo Ramalho <emmanuellramalho@gmail.com>
2018-11-27 03:01:24 -0300

    projeto 2º unidade DotNet

---
+++ DotNet/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNet.Models
+{
+    public class Pessoa
+    {
+        public int PessoaID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string DataNascimento { get; set; }
+        public List<Telefone> Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
---
+++ DotNet/Models/Telefone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNet.Models
+{
+    public class Telefone
+    {
+        public int TelefoneID { get; set; }
+        public string NumeroTelefone { get; set; }
+        public enum TipoDeContato { telefonefixo, telefonecomercial }
+    }
+}
